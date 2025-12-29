3922dbffaba418e3a6ba0e7ed2c0876b2e45e00a
Anastasios Kolorizos <tasosklrz@gmail.com>
2025-12-29 17:54:05 +0200

    Delete src/gr/aueb/cf/testgit directory

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package gr.aueb.cf.testgit;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- src/gr/aueb/cf/testgit/addApp.java
+++
@@ -1,7 +0,0 @@
-package gr.aueb.cf.testgit;
-
-public class addApp {
-   public static void main() {
-        System.out.println( add(1,2));
-    }
-}
